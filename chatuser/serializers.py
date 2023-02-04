87898c34f2aaae8cd7ae68b0d6890d3e3c02a6e7
Rajrup Das <rajrupdasid@keemail.me>
2023-02-04 23:04:17 +0530

    serializers file clipped

---
+++ chatuser/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .models import Message
+from rest_framework.serializers import ModelSerializer, CharField
+
+class MessageSeializer(ModelSerializer):
+    user = CharField(source='user.username',read_only=True)
+    recpient = CharField(source='recipient.username')
+
+    def create(self, validated_data):
+        user = self.context[
+            'request'
+        ].user
+        recipient = get_object_or_404(
+            User, username = validated_data['recipient']['username'])
+
+        msg = Message(recipient=recipient, body=validated_data['body'], user=user)
+        msg.save()
+        return msg
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',)
