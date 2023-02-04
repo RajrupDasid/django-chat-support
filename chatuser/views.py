5fee3e9759d68f7aaf2bbc3762adb2e01f7730d5
Rajrup Das <rajrupdasid@keemail.me>
2023-02-04 23:29:53 +0530

    views api based logic written

--- chatuser/views.py
+++ chatuser/views.py
@@ -2,10 +2,51 @@ from django.shortcuts import render
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
-from rest_framework.pagination import PageNumberpagination
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import SessionAuthentication
 from django.conf import settings
-
+from .serializers import MessageSeializer, UserSerializer
+from .models import Message
 # Create your views here.
+
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+  
+
+    def enforce_csrf(self, request):
+        return
+
+
+class MessagePagination(PageNumberPagination):
+    """
+    Limit message prefetch to one page.
+    """
+    page_size = settings.MESSAGES_TO_LOAD
+
+class MessageViewSet(ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSeializer
+    allowed_methods = ('GET','POST','HEAD','OPTIONS')
+    authentication_classes = (CsrfExemptSessionAuthentication)
+    pagination_class =  MessagePagination
+
+    def list(self, request, *args, **kwargs):
+        self.queryset =self.queryset.filter(Q(recipient = request.user) | (Q(user=request.user)))
+        target = self.queryset.query_params.get('target',None)
+        if target is not None:
+            self.queryset = self.queryset.filter(
+                Q(recipient =  request.user, user__username= target) |
+                Q(recipient__username = target, user=request.user)
+            )
+        return super(MessageViewSet, self).list(request, *args, **kwargs)
+    def retrieve(self, request, *args, **kwargs):
+        msg = get_object_or_404(
+            self.queryset.filter(
+                Q(recipient=request.user) | Q(user=request.user) |
+                Q(pk=kwargs['pk'])
+            )
+        )
+        serializer = self.get_serializer(msg)
+        return Response(serializer.data)
