f632b29f8a9227c80d2db9abbac9cde839b15022
Rajrup Das <rajrupdasid@keemail.me>
2023-02-05 11:06:33 +0530

    asgi

--- core/asgi.py
+++ core/asgi.py
@@ -9,8 +9,31 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from django.urls import path
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+django_asgi_app =  get_asgi_application()
+
+from chatuser.consumers import ChitChat
+
+
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    path(r"^ws",ChitChat.as_asgi())
+                ]
+            )
+        )
+    )
+
+})
