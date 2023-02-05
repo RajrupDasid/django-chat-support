66bf02d32a30f87aaf2d7d65f18e8a1d63effdb5
Rajrup Das <rajrupdasid@keemail.me>
2023-02-05 11:05:17 +0530

    user admin

--- chatuser/admin.py
+++ chatuser/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import ModelAdmin, site
+from .models import Message
+
+
+#class MessageModelAdmin(ModelAdmin):
+#    readonly_fields = ('timestamp',)
+#    search_fields = ('id', 'body', 'user__username', 'recipient__username',)
+#    list_display = ('id', 'user', 'recipient', 'timestamp', 'char_count',)
+#    list_display_links = ('id',)
+#    list_filter = ('user', 'recipient',)
+#
+#
+admin.site.register(Message)
